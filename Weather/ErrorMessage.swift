cef434652cf4ee679f54a339204e34edff58de40
RaeHoon Baek <bmp117@naver.com>
2022-08-10 23:22:53 +0900

    Weather 예제 - 기능 구현하기 - 2

---
+++ Weather/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by 백래훈 on 2022/08/10.
+//
+
+import Foundation
+
+struct ErrorMessage: Codable {
+    let message: String
+}
--- Weather/WeatherInformation.swift
+++ Weather/WeatherInformation.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct weatherInformation: Codable {
+struct WeatherInformation: Codable {
     let weather: [Weather]
     let temp: Temp
     let name: String
